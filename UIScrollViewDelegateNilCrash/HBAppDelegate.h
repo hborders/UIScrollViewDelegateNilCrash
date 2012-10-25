e3f3af66b0e1d6ba315a7e8580bbc9e8f97c8907
Heath Borders <heath.borders@asolutions.com>
2012-10-25 13:22:25 -0500

    Initial Commit

---
+++ UIScrollViewDelegateNilCrash/HBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HBAppDelegate.h
+//  UIScrollViewDelegateNilCrash
+//
+//  Created by dev1 on 10/25/12.
+//  Copyright (c) 2012 Heath Borders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
